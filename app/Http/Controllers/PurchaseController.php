b607e4e4af4a507c20b5b79310282050d1367161
sazukakeiichiro <sdk.kicr7@icloud.com>
2022-11-17 23:18:42 +0900

    sync

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -9,6 +9,7 @@ use Inertia\Inertia;
 use App\Models\Customer;
 use App\Models\Item;
 use Illuminate\Support\Facades\DB; 
+use App\Models\Order;
 
 
 class PurchaseController extends Controller
@@ -20,7 +21,18 @@ class PurchaseController extends Controller
      */
     public function index()
     {
-        //
+        // dd(Order::paginate(50));
+        $orders = Order::groupBy('id')
+        ->selectRaw('id, sum(subtotal) as total,
+        customer_name, status, created_at')
+        ->paginate(50);
+
+        // dd($orders);
+
+        return Inertia::render('Purchases/Index', [
+            'orders' => $orders
+        ]);
+
     }
 
     /**
@@ -84,7 +96,21 @@ class PurchaseController extends Controller
      */
     public function show(Purchase $purchase)
     {
-        //
+        //小計
+        $items = Order::where('id', $purchase->id)
+        ->get();
+        //合計
+        $order = Order::groupBy('id')
+        ->where('id', $purchase->id)
+        ->selectRaw('id, sum(subtotal) as total,
+        customer_name, status, created_at')
+        ->get();
+
+        // dd($items, $order);
+        return Inertia::render('Purchases/Show',[
+            'items' => $items,
+            'order' => $order
+        ] );
     }
 
     /**
@@ -95,8 +121,42 @@ class PurchaseController extends Controller
      */
     public function edit(Purchase $purchase)
     {
-        //
-    }
+        $purchase = Purchase::find($purchase->id);
+
+        $allItems = Item::select('id','name','price')->get();
+
+        $items = [];
+
+        foreach($allItems as $allItem){
+            $quantity = 0; // 数量初期値 0
+            foreach($purchase->items as $item){ // 中間テーブルを1件ずつチェック
+            if($allItem->id === $item->id){ // 同じidがあれば
+                $quantity = $item->pivot->quantity;
+              } }
+
+            array_push($items, [
+            'id' => $allItem->id,
+             'name' => $allItem->name,
+            'price' => $allItem->price,
+             'quantity' => $quantity
+             ]);
+             }
+
+            $order = Order::groupBy('id')
+            ->where('id', $purchase->id)
+            ->selectRaw('id, customer_id,
+            customer_name, status, created_at')
+            ->get();
+
+            return Inertia::render('Purchases/Edit', [
+                'items' => $items,
+                'order' => $order
+            ]);
+
+            
+            
+            }
+    
 
     /**
      * Update the specified resource in storage.
@@ -107,8 +167,32 @@ class PurchaseController extends Controller
      */
     public function update(UpdatePurchaseRequest $request, Purchase $purchase)
     {
-        //
+        DB::beginTransaction();
+
+        try{
+        // dd($request,$purchase);
+        $purchase->status = $request->status;
+        $purchase->save();
+
+        $items = [];
+        foreach($request->items as $item)
+        {
+        $items = $items + [
+        // item_id => [ 中間テーブルの列名 => 値 ]
+        $item['id'] => [ 'quantity' => $item['quantity']]
+        ];
+        }
+
+        // dd($items);
+        $purchase->items()->sync($items);
+
+        DB::commit();
+        return to_route('dashboard');
+    } catch(\Exception $e){
+        DB::rollback();
+
     }
+}
 
     /**
      * Remove the specified resource from storage.
